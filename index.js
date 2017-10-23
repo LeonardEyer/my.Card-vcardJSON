dafbb18b88a920b650b2f2b454c978181a26f21a
Leonard Eyer <leonard.eyer@icloud.com>
2017-10-23 15:21:13 +0200

    prepare index to be used as npm module

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
 const vCard = require('vcf')
 
-const { promisifyAll } = require('bluebird')
-const { readFileAsync, writeFileAsync } = promisifyAll(require('fs'))
 const { singular } = require('pluralize')
 const { matcherFunctions } = require('./src/matcher')
 const { constructName } = require('./src/contactNameConstructor')
 
-const readJSON = identifier => readFileAsync('./cards/' + identifier + '.json').then(JSON.parse)
-const writeVCF = (identifier, data) => writeFileAsync('./cards/' + identifier + '.vcf', data)
 const isArray = a => (!!a) && (a.constructor === Array)
 
 vCard.prototype.addPropObj = function ({ key, value, params = {} }) {
   this.add(key, value, params)
 }
 
-const main = async () => {
+const parseMyCardJSON = async (cardJSON) => {
 
-  const sampleCardIdentifier = 'MyCard-JSON-Contact-Format-Prototype'
-  const cardJSON = await readJSON(sampleCardIdentifier)
   const { contact } = cardJSON
-
   const vcf = new vCard()
 
+  // Construct contact name
   vcf.add('n', constructName(contact))
-  
+
   for (var contactProperty in contact) {
 
     const element = contact[contactProperty]
-
     if (isArray(element)) {
 
+      // Generate props of every element in array and add it to our vcf
       element
         .map(profile => matcherFunctions[singular(contactProperty)](profile))
         .map(prop => vcf.addPropObj(prop))
 
     } else if (matcherFunctions.hasOwnProperty(contactProperty)) {
 
+      // Add all other properties
       const prop = matcherFunctions[contactProperty](element)
       vcf.addPropObj(prop)
     }
   }
 
-  const newCardIdentifier = sampleCardIdentifier
-  writeVCF(newCardIdentifier, vcf.toString('3.0'))
-  console.log(vcf.toString('3.0'))
+  return vcf
 }
 
-main()
+module.exports = { parseMyCardJSON }
